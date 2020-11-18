59a8f0ee05752935c1447a748ee0e65a8c724d05
hohenheim99 <74414862+hohenheim99@users.noreply.github.com>
2020-11-18 19:12:16 -0300

    commit

--- eaglecopters.sql
+++ eaglecopters.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2020 a las 21:53:47
+-- Tiempo de generación: 18-11-2020 a las 23:11:49
 -- Versión del servidor: 10.4.16-MariaDB
 -- Versión de PHP: 7.4.12
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `administrador` (
   `ID` int(11) NOT NULL,
   `nombre` varchar(256) NOT NULL,
-  `datosID` varchar(50) NOT NULL
+  `datosID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -42,10 +42,10 @@ CREATE TABLE `administrador` (
 CREATE TABLE `cliente` (
   `ID` int(11) NOT NULL,
   `Nombre` varchar(256) NOT NULL,
-  `EmpresaID` int(11) NOT NULL,
-  `Objetivo` varchar(256) NOT NULL,
-  `Tiempo` int(11) NOT NULL,
-  `Apuntes` varchar(1024) DEFAULT NULL
+  `empresaID` int(11) DEFAULT NULL,
+  `objetivoID` int(11) DEFAULT NULL,
+  `Tiempo` date NOT NULL,
+  `Apuntes` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -57,7 +57,7 @@ CREATE TABLE `cliente` (
 CREATE TABLE `ejecutivos` (
   `ID` int(11) NOT NULL,
   `nombre` varchar(256) NOT NULL,
-  `datosID` varchar(50) NOT NULL
+  `datosID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -69,7 +69,7 @@ CREATE TABLE `ejecutivos` (
 CREATE TABLE `empresa` (
   `ID` int(11) NOT NULL,
   `nombre` varchar(256) NOT NULL,
-  `rubroID` varchar(50) NOT NULL
+  `rubroID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -125,7 +125,8 @@ ALTER TABLE `administrador`
 --
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `EmpresaID` (`EmpresaID`);
+  ADD KEY `empresaID` (`empresaID`),
+  ADD KEY `objetivoID` (`objetivoID`);
 
 --
 -- Indices de la tabla `ejecutivos`
@@ -164,6 +165,42 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ejecutivos`
+--
+ALTER TABLE `ejecutivos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `helicopteros`
+--
+ALTER TABLE `helicopteros`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rubros`
+--
+ALTER TABLE `rubros`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -177,25 +214,26 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `administrador`
 --
 ALTER TABLE `administrador`
-  ADD CONSTRAINT `administrador_ibfk_1` FOREIGN KEY (`datosID`) REFERENCES `users` (`ID`);
+  ADD CONSTRAINT `administrador_ibfk_1` FOREIGN KEY (`datosID`) REFERENCES `usuarios` (`ID`);
 
 --
 -- Filtros para la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`EmpresaID`) REFERENCES `empresa` (`ID`);
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`empresaID`) REFERENCES `empresa` (`ID`),
+  ADD CONSTRAINT `cliente_ibfk_2` FOREIGN KEY (`objetivoID`) REFERENCES `helicopteros` (`ID`);
 
 --
 -- Filtros para la tabla `ejecutivos`
 --
 ALTER TABLE `ejecutivos`
-  ADD CONSTRAINT `ejecutivos_ibfk_1` FOREIGN KEY (`datosID`) REFERENCES `users` (`ID`);
+  ADD CONSTRAINT `ejecutivos_ibfk_1` FOREIGN KEY (`datosID`) REFERENCES `usuarios` (`ID`);
 
 --
 -- Filtros para la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  ADD CONSTRAINT `empresa_ibfk_1` FOREIGN KEY (`rubroID`) REFERENCES `rubro` (`ID`);
+  ADD CONSTRAINT `empresa_ibfk_1` FOREIGN KEY (`rubroID`) REFERENCES `rubros` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
