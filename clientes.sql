988b7ab1adcfa1c62e4f3b4bf12444bfa423caa1
hohenheim99 <74414862+hohenheim99@users.noreply.github.com>
2020-11-18 17:55:15 -0300

    commit

--- clientes.sql
+++ clientes.sql
@@ -1,51 +1,43 @@
+CREATE DATABASE IF NOT EXISTS Eaglecopters;
 
+USE Eaglecopters;
 
-CREATE DATABASE IF NOT EXISTS 'BD_Eaglecopters';
-
-USE BD_Eaglecopters;
-
-
--- Create a new table called 'admin' in schema 'DB_Eaglecopters'
--- Drop the table if it already exists
-
-
-CREATE TABLE IF NOT EXISTS cliente
-(
-    clienteID INT NOT NULL PRIMARY KEY, -- primary key column
-    Nombre [NVARCHAR](256) NOT NULL,
-    FOREIGN KEY (EmpresaID) REFERENCES Empresas(EmpresaID)
-    --Empresa [NVARCHAR](256) NOT NULL,
-    Objetivo [NVARCHAR](256) NOT NULL,
-    Tiempo INT NOT NULL,
-    Apuntes [NVARCHAR](1024)
+CREATE TABLE IF NOT EXISTS Cliente(
+ID INT NOT NULL AUTO_INCREMENT,
+Nombre VARCHAR(256) NOT NULL,
+EmpresaID INT NOT NULL,
+Objetivo VARCHAR(256) NOT NULL,
+Tiempo INT NOT NULL,
+Apuntes VARCHAR(1024),
+PRIMARY KEY (ID),
+FOREIGN KEY (EmpresaID) REFERENCES Empresa(ID)
     
 );
 
 
--- Create a new table called 'Ejecutivos' in schema 'DB_Eaglecopters'
--- Drop the table if it already exists
+
 
 CREATE TABLE IF NOT EXISTS Ejecutivos
 (
-    EmpresaID INT NOT NULL PRIMARY KEY, -- primary key column
-    nombre [NVARCHAR](256) NOT NULL,
-    FOREIGN KEY(datosID) REFERENCES users(usersID)
-    --correo [NVARCHAR](50) NOT NULL,
-    --pass [NVARCHAR](256) NOT NULL
+    ID INT NOT NULL AUTO_INCREMENT, 
+    nombre VARCHAR(256) NOT NULL,
+    datosID_eje INT NOT NULL,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(datosID_eje) REFERENCES Users(ID)
     
 );
 
 
--- Create a new table called 'Administrador' in schema 'DB_Eaglecopters'
--- Drop the table if it already exists
+
 
 CREATE TABLE IF NOT EXISTS Administrador
 (
-    adminID INT NOT NULL PRIMARY KEY, -- primary key column
-    nombre [NVARCHAR](50) NOT NULL,
-    FOREIGN  KEY(datosID) REFERENCES users(usersId)
-    --correo [NVARCHAR](50) NOT NULL,
-    --pass  [NVARCHAR](50) NOT NULL
+    ID INT NOT NULL AUTO_INCREMENT, 
+    nombre VARCHAR(256) NOT NULL,
+    datosID_admin INT NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN  KEY(datosID_admin) REFERENCES Users(ID)
+   
     
 );
 
@@ -53,44 +45,46 @@ CREATE TABLE IF NOT EXISTS Administrador
 
 CREATE TABLE IF NOT EXISTS Empresa
 (
-    EmpresaID INT NOT NULL PRIMARY KEY, -- primary key column
-    nombre [NVARCHAR](256) NOT NULL,
-    FOREIGN KEY (rubroID) REFERENCES rubro(rubroID)
+    ID INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(256) NOT NULL,
+    rubroID INT NOT NULL,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(rubroID) REFERENCES Rubros(ID)
     
 );
 
 
-CREATE TABLE DB_Eaglecopters.rubro
+CREATE TABLE IF NOT EXISTS Rubros
 (
-    rubroID INT NOT NULL PRIMARY KEY, -- primary key column
-    nombre [NVARCHAR](256) NOT NULL,
-    tipo [NVARCHAR](256) NOT NULL
+    ID INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(256) NOT NULL,
+    tipo VARCHAR(256) NOT NULL,
+    PRIMARY KEY(ID)
     
 );
-GO
 
--- Create a new table called 'Helicopteros' in schema 'DB_Eaglecopters'
--- Drop the table if it already exists
+
+
 
 CREATE TABLE IF NOT EXISTS Helicopteros
 (
-    HeliId INT NOT NULL PRIMARY KEY, -- primary key column
-    modelo [NVARCHAR](50) NOT NULL,
-    marca [NVARCHAR](50) NOT NULL,
-    patente_tipo [NVARCHAR](50) NOT NULL
+    ID INT NOT NULL AUTO_INCREMENT,
+    modelo VARCHAR(50) NOT NULL,
+    marca VARCHAR(50) NOT NULL,
+    patente_tipo VARCHAR(50) NOT NULL,
+    PRIMARY KEY(ID)
     
 );
-GO
-
--- Create a new table called 'users' in schema 'BD_Eaglecopters'
--- Drop the table if it already exists
 
 
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE IF NOT EXISTS Users
 (
-    usersId INT NOT NULL PRIMARY KEY, -- primary key column
-    correo [NVARCHAR](50) NOT NULL,
-    pass [NVARCHAR](50) NOT NULL
+    ID INT NOT NULL AUTO_INCREMENT, 
+    mail VARCHAR(50) NOT NULL,
+    pass VARCHAR(256) NOT NULL,
+    PRIMARY KEY (ID)
     
 );
+
+
 
---
+++ eaglecopters_phpmyadmin.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-11-2020 a las 21:53:47
+-- Versión del servidor: 10.4.16-MariaDB
+-- Versión de PHP: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `eaglecopters`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `ID` int(11) NOT NULL,
+  `nombre` varchar(256) NOT NULL,
+  `datosID` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(256) NOT NULL,
+  `EmpresaID` int(11) NOT NULL,
+  `Objetivo` varchar(256) NOT NULL,
+  `Tiempo` int(11) NOT NULL,
+  `Apuntes` varchar(1024) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ejecutivos`
+--
+
+CREATE TABLE `ejecutivos` (
+  `ID` int(11) NOT NULL,
+  `nombre` varchar(256) NOT NULL,
+  `datosID` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa`
+--
+
+CREATE TABLE `empresa` (
+  `ID` int(11) NOT NULL,
+  `nombre` varchar(256) NOT NULL,
+  `rubroID` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `helicopteros`
+--
+
+CREATE TABLE `helicopteros` (
+  `ID` int(11) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `patente_tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rubros`
+--
+
+CREATE TABLE `rubros` (
+  `ID` int(11) NOT NULL,
+  `nombre` varchar(256) NOT NULL,
+  `tipo` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID` int(11) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `pass` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `datosID` (`datosID`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `EmpresaID` (`EmpresaID`);
+
+--
+-- Indices de la tabla `ejecutivos`
+--
+ALTER TABLE `ejecutivos`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `datosID` (`datosID`);
+
+--
+-- Indices de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `rubroID` (`rubroID`);
+
+--
+-- Indices de la tabla `helicopteros`
+--
+ALTER TABLE `helicopteros`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `rubros`
+--
+ALTER TABLE `rubros`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD CONSTRAINT `administrador_ibfk_1` FOREIGN KEY (`datosID`) REFERENCES `users` (`ID`);
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`EmpresaID`) REFERENCES `empresa` (`ID`);
+
+--
+-- Filtros para la tabla `ejecutivos`
+--
+ALTER TABLE `ejecutivos`
+  ADD CONSTRAINT `ejecutivos_ibfk_1` FOREIGN KEY (`datosID`) REFERENCES `users` (`ID`);
+
+--
+-- Filtros para la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD CONSTRAINT `empresa_ibfk_1` FOREIGN KEY (`rubroID`) REFERENCES `rubro` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
